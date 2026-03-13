f9d2cc5743ad2013c045b3e80b1c57fbe6aa8616
tiagokta01@gmail.com <CAMPUSRP-LAB\18165464612@rpbdel3am9-l.campusrp-lab.riopomba.ifsudestemg.edu.br>
2026-03-13 14:35:37 -0300

    mgh

---
+++ exercicios.py
@@ -0,0 +1,94 @@
+'''#lista
+notas = [7.5, 8.0, 6.0, 9.5, 5.0]
+
+#a
+notas.append(8.5)
+
+#b
+notas = notas.remove(5.0)
+notas = notas.append(6.5)
+
+#c
+notas.sort(reverse=True)
+print("Lista ordenada (maior para menor):", notas)
+
+#d
+print("Maior nota:", max(notas))
+print("Menor nota:", min(notas))'''
+
+#exercicio2
+
+'''colegas = ['João', 'Milena', 'Pedro', 'isabela', 'Lucas']
+
+for i, nome in enumerate(colegas):
+    print(f'Aluno {i+1}: {nome}')'''
+
+#exercicio3
+'''numeros = [3, 17, 8, 42, 5, 100, 23, 66, 11, 99]
+
+print("Números pares:")
+for n in numeros:
+    if n % 2 == 0:
+        print(n)
+
+maiores_que_20 = []
+
+for n in numeros:
+    if n > 20:
+        maiores_que_20.append(n)
+
+print("Números maiores que 20:", maiores_que_20)
+
+soma = sum(numeros)
+print("Soma dos números:", soma)'''
+
+#exercicio4
+
+'''numeros = list(range(1, 11))
+
+print(numeros)
+
+print(numeros[:4])
+
+print(numeros[-3:])
+
+print(numeros[::2])'''
+
+#exercicio5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
